00cf268aed0db31d88f45b1db500f0929646d44e
Ankit Panda <ankitpanda@Ankits-MacBook-Air.local>
2023-09-08 23:24:13 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 import  Home from "./Components/Home/Home";
-import Homepage from "./Components/Home/Navbar/NewsGround/free.js"
+
 import './App.css';
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM, {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from './Components/Home/Home.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
